ae5cb071dc7be5c4dd7ec5ce9a69a51aacbaca8b
jeelpatel0362 <jeelpatel0362@gmail.com>
2024-12-13 10:11:50 +0530

    Digital card

--- app/src/main/java/com/example/digitalcard/InformationPage.java
+++ app/src/main/java/com/example/digitalcard/InformationPage.java
@@ -1,6 +1,9 @@
 package com.example.digitalcard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,15 @@ public class InformationPage extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_information_page);
 
+        Button nxtButton = findViewById(R.id.nextButton);
+
+        nxtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InformationPage.this,EditDataScreen.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
