ad25c7eb33e7a9e7e4b5ba10d3232d969c4fcb9c
jeelpatel0362 <jeelpatel0362@gmail.com>
2024-12-13 12:58:33 +0530

    Digital card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.9.0")
+
 
 
 }
